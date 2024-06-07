f53e9ea2be2e4a289a5248a8725be8f18611ddc2
Ricardo <rikurida@gmail.com>
2024-06-06 22:05:33 -0400

    wip

--- src/modules/apigateway/main.tf
+++ src/modules/apigateway/main.tf
@@ -27,7 +27,3 @@ resource "aws_apigatewayv2_stage" "default_stage" {
   name        = "$default"
   auto_deploy = true
 }
-
-output "api_url" {
-  value = aws_apigatewayv2_stage.default_stage.invoke_url
-}
